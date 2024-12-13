aa8d86dcd3d4f7be64b52b0ff782a0c83c762a04
gilangfatahilah <gilangf010@gmail.com>
2024-12-13 23:08:49 +0700

    🌈 style(tailwind): add custom classes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { version as reactVersion } from "react";
 export default function App() {
   return (
     <div className="w-full h-[100vh] flex flex-col items-center justify-center">
-      <p className="text-emerald-500 font-bold underline">Hellooo!</p>
+      <p className="text-primary font-bold underline">Hellooo!</p>
       <p className="text-cyan-500">{"React v" + reactVersion} </p>
     </div>
   );
