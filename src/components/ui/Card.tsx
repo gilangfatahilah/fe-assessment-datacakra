ed9f0c4e2917c26026f0de7a31d2183f0dc4f28c
gilangfatahilah <gilangf010@gmail.com>
2024-12-15 23:07:55 +0700

    ✨ feat(component): add card & chart

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,43 @@
+import { HTMLAttributes, PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface Props extends PropsWithChildren, HTMLAttributes<HTMLDivElement> {
+  title?: string;
+  description?: string;
+  imageUrl?: string;
+}
+
+const Card = ({
+  title,
+  description,
+  imageUrl,
+  children,
+  className,
+  ...restProps
+}: Props) => {
+  return (
+    <div
+      className={twMerge(
+        "bg-background border border-border rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300",
+        className
+      )}
+      {...restProps}
+    >
+      {imageUrl && (
+        <img src={imageUrl} alt={title} className="w-full h-48 object-cover" />
+      )}
+
+      <div className="p-4">
+        {title && <h2 className="text-xl font-bold mb-2">{title}</h2>}
+
+        {description && (
+          <p className="mb-4 text-foreground/50">{description}</p>
+        )}
+
+        {children && <div className="mt-2">{children}</div>}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/ui/Chart.tsx
@@ -0,0 +1,74 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar, Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+type ChartType = "bar" | "line";
+
+export interface Props {
+  data: number[];
+  labels: string[];
+  type?: ChartType;
+}
+
+const Chart = ({ data, labels, type = "bar" }: Props) => {
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        title: "Article",
+        data: data,
+        backgroundColor: "rgba(37, 99, 235, 1)",
+        borderColor: "rgba(37, 99, 235, 1)",
+        borderWidth: 2,
+        borderRadius: 5,
+        fill: true,
+      },
+    ],
+  };
+
+  const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+
+  return (
+    <div className="w-full h-[50vh]">
+      <div className="relative w-full h-full">
+        {type === "bar" && <Bar data={chartData} options={chartOptions} />}
+        {type === "line" && <Line data={chartData} options={chartOptions} />}
+      </div>
+    </div>
+  );
+};
+
+export default Chart;
