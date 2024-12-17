082ae1afa73f38aadafbe9c60398dd7975434c53
gilangfatahilah <gilangf010@gmail.com>
2024-12-17 20:49:59 +0700

    🎈 perf(dashboard page): integrate with data from api

--- src/pages/authenticated/DashboardPage.tsx
+++ src/pages/authenticated/DashboardPage.tsx
@@ -1,12 +1,22 @@
 import AuthenticatedLayout from "@/layouts/Authenticated";
 import Card from "@/components/ui/Card";
 import Chart from "@/components/ui/Chart";
+import { useArticleStore } from "@/stores/useArticleStore";
+import { useEffect } from "react";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { Loader } from "lucide-react";
+// import Button from "@/components/ui/Button";
 
 const DashboardPage = () => {
-  const chartData = {
-    data: [100, 200, 150, 300, 250],
-    labels: ["January", "February", "March", "April", "May"],
-  };
+  const { user } = useAuthStore();
+  const { articleChartData, loading, getArticleChartData, reset } =
+    useArticleStore();
+
+  useEffect(() => {
+    if (user) getArticleChartData(user.id);
+
+    return () => reset();
+  }, [getArticleChartData, user, reset]);
 
   return (
     <AuthenticatedLayout>
@@ -49,8 +59,26 @@ const DashboardPage = () => {
           </Card>
         </div>
 
-        <Card title="Most high" className="h-[62vh]">
-          <Chart data={chartData.data} labels={chartData.labels} type="line" />
+        <Card className="h-[62vh]">
+          <h1 className="text-2xl font-bold text-center py-2">
+            Most commented articles
+          </h1>
+
+          {articleChartData?.data.length && !loading ? (
+            <Chart
+              data={articleChartData?.data}
+              labels={articleChartData?.labels}
+              type="bar"
+            />
+          ) : loading ? (
+            <Loader className="animate-spin mx-auto" />
+          ) : (
+            <div className="h-max flex flex-col gap-4 items-center justify-center">
+              <p className="text-center text-muted-foreground font-semibold text-lg">
+                No articles yet.
+              </p>
+            </div>
+          )}
         </Card>
       </div>
     </AuthenticatedLayout>
